44518a25ede9d4785b2398eff059bf620ee03373
dsarthak0 <131682623+dsarthak0@users.noreply.github.com>
2025-03-16 19:39:58 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript can be used for interactivity, but for now, this is a simple structure
+document.addEventListener("DOMContentLoaded", () => {
+    // You can add interactivity here later, like animations, scroll effects, etc.
+  });
+  
